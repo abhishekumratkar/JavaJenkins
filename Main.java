d9c3119541daa634b559ae92b92a8da277d4518b
abhisek <abhishekumratkar2@gmail.com>
2024-05-02 12:19:58 +0530

    This is for Jenkins

---
+++ Main.java
@@ -0,0 +1,11 @@
+import javax.xml.crypto.Data;
+import java.util.Date;
+
+public class Main {
+    public static void main(String args[]){
+        System.out.println("This is the java project");
+        System.out.println(new Date());
+        System.out.println("This is Abhishek");
+
+    }
+}
